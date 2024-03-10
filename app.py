dc52666e41018ea701a9f86114338d8d7e0f2118
Santosh Bhor <91824340+BhorSant@users.noreply.github.com>
2024-03-10 16:56:26 +0530

    app

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import pickle
 import streamlit as st
+import numpy as np
+import seaborn as sns
+st.header("Book Recommendation System using Machine Learning")
+
+st.sidebar.header("User Profile")
+
+user_name = st.sidebar.text_input("Enter your name")
+
+st.sidebar.header("User Profile")
+
+st.sidebar.subheader(user_name)
+# Used model
+
+model = pickle.load(open("artifacts\model.pkl", "rb"))
+books_name = pickle.load(open("artifacts\books.pkl", "rb"))
+book_pivot = pickle.load(open("artifacts\book_pivot.pkl", "rb"))
+final_rating = pickle.load(open("artifacts\final_rating.pkl","rb"))
+
+
